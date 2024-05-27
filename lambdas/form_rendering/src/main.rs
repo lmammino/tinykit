35596f79ad57c9b7dc003f471e725f640b3b18cd
Luciano Mammino <lucianomammino@gmail.com>
2024-05-27 19:20:15 +0100

    implemented first crappy version of the form rendering lambda

--- lambdas/form_rendering/src/main.rs
+++ lambdas/form_rendering/src/main.rs
@@ -1,23 +1,47 @@
-use lambda_http::{run, service_fn, tracing, Body, Error, Request, RequestExt, Response};
+use lambda_http::{
+    request::RequestContext, run, service_fn, tracing, Body, Error, Request, RequestExt,
+    RequestPayloadExt, Response,
+};
 
-/// This is the main body for the function.
-/// Write your code inside it.
-/// There are some code example in the following URLs:
-/// - https://github.com/awslabs/aws-lambda-rust-runtime/tree/main/examples
+fn create_form(target_url: &str) -> String {
+    format!(
+        r#"
+    <html>
+        <head>
+            <title>Subscribe to our newsletter</title>
+        </head>
+        <body>
+            <form action="{target_url}" method="post">
+                <label for="email">Email:</label>
+                <input required type="email" id="email" name="email">
+                <input type="submit" value="Submit">
+            </form>
+        </body>
+    </html>
+    "#
+    )
+}
+
 async fn function_handler(event: Request) -> Result<Response<Body>, Error> {
-    // Extract some useful information from the request
-    let who = event
-        .query_string_parameters_ref()
-        .and_then(|params| params.first("name"))
-        .unwrap_or("world");
-    let message = format!("Hello {who}, this is an AWS Lambda HTTP request");
+    let request_context = event.request_context();
+    let form_submit_url = match request_context {
+        RequestContext::ApiGatewayV2(api_gateway_v2) => {
+            let domain_name = api_gateway_v2.domain_name.expect("domain_name not found");
+            let path = api_gateway_v2.http.path.expect("path not found");
+            format!("https://{}{}", domain_name, path)
+        }
+        _ => {
+            return Err("Unsupported request context".into());
+        }
+    };
+    let form_html = create_form(&form_submit_url);
 
     // Return something that implements IntoResponse.
     // It will be serialized to the right response event automatically by the runtime
     let resp = Response::builder()
         .status(200)
         .header("content-type", "text/html")
-        .body(message.into())
+        .body(form_html.into())
         .map_err(Box::new)?;
     Ok(resp)
 }
